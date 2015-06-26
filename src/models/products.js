575a8332b1c56f99354453385807ccc92f9bc129
Raul Lopez <rldona@gmail.com>
2015-06-26 18:49:03 +0200

    Add new products.js

---
+++ src/models/products.js
@@ -0,0 +1,13 @@
+// Dependencies
+var restful = require('node-restful');
+var mongoose = require('mongoose');
+
+// Schema
+var productSchema = new mongoose.Schema({
+	name: String,
+	sku: String,
+	price: Number
+});
+
+// Return model
+module.exports = restful.model('Products', productSchema);
