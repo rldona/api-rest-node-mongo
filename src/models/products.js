4a9227b4e2cef4c17bfadd73c61e326bff4b7c8e
Raúl López <rldona@gmail.com>
2015-07-12 01:05:39 +0200

    [FIX] apply jshint

--- src/models/products.js
+++ src/models/products.js
@@ -4,9 +4,9 @@ var mongoose = require('mongoose');
 
 // Schema
 var productSchema = new mongoose.Schema({
-	name: String,
-	sku: String,
-	price: Number
+  name: String,
+  sku: String,
+  price: Number
 });
 
 // Return model
--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 // Dependencies
-var express = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
-    app = express();
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express();
 
 // MongoDB
 mongoose.connect('mongodb://localhost/rest_test');
 
 // Express config
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
+
 app.use(bodyParser.json());
 
 // Routes
