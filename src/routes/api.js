8d5aea054762d7a6bdaa9afe5e3a96c04460daa3
Raúl López <rldona@gmail.com>
2015-07-13 20:41:38 +0200

    [FIX] add semicolon

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
-// Models
-var Product = require('../models/products')
+// Model
+var Product = require('../models/products');
 
 // Routes
 Product.methods(['get', 'put', 'post', 'delete']);
-Product.register(router, '/products')
+Product.register(router, '/products');
 
 // Return router
 module.exports = router;
