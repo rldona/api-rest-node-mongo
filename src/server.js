c6674b693b9d5aa936f4f220d84da3b81e681dee
Raúl López <rldona@gmail.com>
2015-08-10 00:03:29 +0200

    Added support for heroku

--- src/server.js
+++ src/server.js
@@ -3,10 +3,21 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
+
+// Server Port
 var port = process.env.PORT || 5050;
 
+// MONGOLAB_URI & MONGOHQ_URL are necessary to MongoLab (Heroku)
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/products';
+
 // MongoDB
-mongoose.connect('mongodb://localhost/products');
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '.' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 // Express config
 app.use(bodyParser.urlencoded({ extended: true }));
